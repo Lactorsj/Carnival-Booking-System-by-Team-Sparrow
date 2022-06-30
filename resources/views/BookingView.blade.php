0bbc3f2429ce53c84b2817dfe5fb75f2de79fd02
chenghanchang <c0628chc@163.com>
2022-06-30 08:22:22 +0800

    improve the functions on front page of bookingview

--- resources/views/BookingView.blade.php
+++ resources/views/BookingView.blade.php
@@ -111,6 +111,27 @@
             </div>
         </div><br>
         @endif
+
+        @if($_ENV['CURRENT_DAY'] == $_ENV['CARNIVAL_DAYS'])
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                    无日期可预约
+                    <a href="http://localhost:8000/dashboard">
+                        <button type="button" style="float:right">Return Dashboard</button>
+                    </a>
+                </div>
+            </div>
+        </div><br>
+        @else
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8" hidden="hidden">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 bg-white border-b border-gray-200">
+                无日期可预约
+                </div>
+            </div>
+        </div><br>
+        @endif
     </div>
 </x-app-layout>
 </html>
